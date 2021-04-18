3976d7f88e10e53bfb9900f472b91f54e2eb858c
1633745323@qq.com <1633745323@qq.com>
2021-04-18 14:25:07 +0800

    标签页管理更加人性化了

--- src/com/tool/TabManager.java
+++ src/com/tool/TabManager.java
@@ -11,21 +11,47 @@ public class TabManager
 			MainWindow.tabbedPane.remove(index);
 		} else
 		{
-			closeOtherTab();
+			closeAllCodeTab();
 		}
 	}
 
-	public static void closeAllTab()
+	public static void closeAllTab() // 包括自动审计面板
 	{
 		MainWindow.tabbedPane.removeAll();
 		AutoCheckManager.AutoCheckInit();
 	}
 
-	public static void closeOtherTab()
+	public static void closeAllCodeTab()
 	{
 		for (int i = MainWindow.tabbedPane.getTabCount() - 1; i > 0; i--)
 		{
 			MainWindow.tabbedPane.remove(i);
 		}
 	}
+
+	public static void closeOtherTab()
+	{
+		if (MainWindow.tabbedPane.getSelectedIndex() != 0)
+		{
+			closeAllRightTab();
+			closeAllleftTab();
+		}
+	}
+
+	public static void closeAllRightTab()
+	{
+		for (int i = MainWindow.tabbedPane.getTabCount() - 1; i > MainWindow.tabbedPane.getSelectedIndex(); i--)
+		{
+			MainWindow.tabbedPane.remove(i);
+		}
+	}
+
+	public static void closeAllleftTab()
+	{
+		for (int i = MainWindow.tabbedPane.getSelectedIndex() - 1; i > 0; i--)
+		{
+			MainWindow.tabbedPane.remove(i);
+		}
+	}
+
 }
